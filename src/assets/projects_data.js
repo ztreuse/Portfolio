449bcc877f4bd374a3dd0a66aabb5ca688c82e51
ztreuse <tjzbay13@gmail.com>
2025-12-24 03:01:41 +0800

    ecocool upload

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -1,6 +1,7 @@
 import tipsytavern from './projects/tipsy.png';
 import ticap from './projects/tcp2.png';
 import tourisla from './projects/tourisla.png';
+import ecocool from './projects/ecocool.png';
 
 const Projects_Data = [
     {
@@ -20,9 +21,16 @@ const Projects_Data = [
     {
         p_img: tourisla,
         p_name:"Tourisla",
-        p_desc:"A Web Platform for Tourism Management with an Interactive Map and a Mobile App for Booking Tour Guides and Local Culture Promotion to Enhance Visitor Experience in Bantayan, Cebu",
+        p_desc:"A Web Platform for Tourism Management with an Interactive Map and a Mobile App for Booking Tour Guides and Local Culture Promotion to Enhance Visitor Experience in Bantayan, Cebu.",
         p_tools: ["React.js", "React Native", "PostgreSQL", "Node.js", "Express.js", "Tailwind CSS", "ShadCN/ui"],
         p_link:"https://tourisla.space/"
+    },
+    {
+        p_img: ecocool,
+        p_name:"Eco-Cool",
+        p_desc:"Developed a high-fidelity Figma mockup for Eco Cool, an eco-conscious brand that provides cooling relief in the Philippines while promoting reforestation through mini fans equipped with plant seeds.",
+        p_tools: ["Figma", "Photoshop", "Canva"],
+        p_link:"https://www.figma.com/proto/HLNCpuLjMihp36PbUrBKOo/ECO-COOL-DRAFT?node-id=77-720&p=f&t=Q0pHWvJxtKTeO1QA-0&scaling=contain&content-scaling=fixed&page-id=0%3A1"
     },
 ]
  
